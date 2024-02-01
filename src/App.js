bc4c9cb6eec0a472aaed5fecd2fba0847349ef31
gauri <atharvashirkande7070@gmail.com>
2024-02-01 13:43:08 +0530

    Modifying the color

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Welcome to the world of React!</h1>
+          <h2>Welcome to the world of React!</h2>
         </p>
         <a
           className="App-link"
